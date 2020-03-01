e32b635d3fa97a2c247eeced318d99879973f7e0
Evan Downton <evan.downton@gmail.com>
2020-03-01 13:09:11 -0500

    Added testMany method and finalized code

--- HW1/src/QueueTest.java
+++ HW1/src/QueueTest.java
@@ -34,25 +34,40 @@ public class QueueTest {
 
         //Both queues are empty
         System.out.println("EMPTY QUEUE TEST");
-        if (testEmpty(new ListQueue(), new JavaQueue())) { //Tests if an empty java queue is the same as our queue
-            System.out.println("The two Queues are the same");
-        }
+        testEmpty(new ListQueue(), new JavaQueue()); //Tests if an empty java queue is the same as our queue
 
         //Both queues starting with 1 element
         ListQueue list = new ListQueue("Zeta");
         JavaQueue java = new JavaQueue();
         java.enqueue("Zeta");
 
-        System.out.println("ONE ELEMENT QUEUE TEST");
+        System.out.println("\nONE ELEMENT QUEUE TEST");
         testOne(list, java);
-        //testMany(new ListQueue(),new JavaQueue());
+
+        System.out.println("\nMANY ELEMENT QUEUE TEST");
+        ListQueue list2 = new ListQueue("Demo");
+        JavaQueue java2 = new JavaQueue();
+        java2.enqueue("Demo");
+        list2.enqueue("Demo2");
+        java2.enqueue("Demo2");
+        list2.enqueue("Demo3");
+        java2.enqueue("Demo3");
+        testMany(list2 ,java2);
 
     }
 
     public static boolean testEmpty(ListQueue yourQueue, JavaQueue correctQueue) {
         if (yourQueue.front() != correctQueue.front()) { //Tests to see if the the empty queues are equal
+            System.out.println("Both queues are not the same");
             return false;
         }
+        yourQueue.enqueue("test");              //Tests to see if queues remain the same even after insertion
+        correctQueue.enqueue("test");               //Effectively this is here to test enqueue and dequeue methods
+        if(yourQueue.dequeue() != correctQueue.dequeue()){
+            System.out.println("Both queues are not the same");
+            return false;
+        }
+        System.out.println("The two Queues are the same"); //reports that the two queues were found to be the same before returning from the method back to main
         return true; //If the code makes it to this point then the queues were the same at all points.
     }
 
@@ -78,9 +93,26 @@ public class QueueTest {
     }
 
     public static boolean testMany(ListQueue a, JavaQueue b) {
-        //TODO implement a test of the three functions when the queues have many elements
-        //Compare the result of your queue  against the java implementation
-        //More than one test may be necessary
-        return false;
+        a.enqueue("Demo4"); //These two lines serve to test the enqueue function
+        b.enqueue("Demo4"); //Both queues are now set up and can be tested
+
+        while(a.front() != null){ //This loop checks to make sure that all the contents of the two arrays match
+            if(b.front() == null){ //If b's front returns a null value it means the length of the two queues are different and therefore are not the same
+                System.out.println("The queues are not the same"); //Placing this check before the next prevents an error form being thrown from trying to dequeue a non existent value
+                return false;
+            }
+            if(a.dequeue() != b.dequeue()){ //If the contents of the first value in each queue are different than they are not the same and the method returns
+                System.out.println("The queues are not the same");
+                return false;
+            }
+        }
+
+        if(b.front() != null){ //If there is still a value in B after clearing out A than B has a different length than A and is inherently different and not the same
+            System.out.println("The two queues are not the same");
+            return false;
+        }
+
+        System.out.println("The two queues are the same");//The the program makes it to this point without returning then the two queues must be the same
+        return true;
     }
 }
